59c95dce9e4191c8aefc9ff5ec7b09c9ad8bd263
OTS0 <79659545+OTS0@users.noreply.github.com>
2021-06-05 00:17:13 +0300

    update

--- Server/src/Main.java
+++ Server/src/Main.java
@@ -13,5 +13,6 @@ public class Main {
      */
     public static void main(String[] args) throws IOException, ClassNotFoundException, IdException, CollectionException, NullException, IndexNotFoundException {
         Server server = new Server();
+        System.out.println("Ура");
     }
 }
